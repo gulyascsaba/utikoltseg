96faf4cc4f67eaf6aa2a815e22a33433006291d6
Csaba <gulyasscsaba@gmail.com>
2022-02-21 13:00:51 +0100

    nyolc

---
+++ script.js
@@ -0,0 +1,11 @@
+function Nyom(){
+var kilometer = document.getElementById("ossztav").value;
+var uzemanyagar = document.getElementById("uzemanyagar").value;
+
+var utikoltseg=8*uzemanyagar*kilometer/100;
+document.getElementById("1").innerHTML=utikoltseg;
+var amortizacio=kilometer*15;
+document.getElementById("2").innerHTML=amortizacio;
+var osszkoltseg=amortizacio+utikoltseg;
+document.getElementById("3").innerHTML=osszkoltseg;
+}
