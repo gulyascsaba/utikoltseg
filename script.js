541d90186c1e8e43a073e7cc64fa1831d2044b22
Csaba <gulyasscsaba@gmail.com>
2022-02-21 13:03:04 +0100

    9

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function Nyom(){
 var kilometer = document.getElementById("ossztav").value;
 var uzemanyagar = document.getElementById("uzemanyagar").value;
-
 var utikoltseg=8*uzemanyagar*kilometer/100;
 document.getElementById("1").innerHTML=utikoltseg;
 var amortizacio=kilometer*15;
